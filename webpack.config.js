25fdce7f3bd551b23236d27ac84b3d297a30acd9
DESKTOP-PULU5LM\Meisam <meisam.ahmadi.1358@gmail.com>
2018-07-22 22:13:49 +0430

    Composing Configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,30 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ErrorOverlayPlugin = require("error-overlay-webpack-plugin");
 const webpack = require("webpack");
 const path = require("path");
+const merge = require("webpack-merge");
 
-module.exports = () => {
-    return {
+
+const commonConfig = merge([
+    {
         plugins: [
             new HtmlWebpackPlugin({title: "webpack demo"}),
             new ErrorOverlayPlugin(),
             // Ignore node_modules so CPU usage with poll watching drops significantly.
             new webpack.WatchIgnorePlugin([path.resolve(__dirname, "node_modules")]),
         ],
-        devServer: {
-            stats: "errors-only", // Display only errors to reduce the amount of output.
-            host: process.env.HOST, // Defaults to `localhost`
-            port: process.env.PORT || 3000, // Defaults to 8080
-            open: true, // Open the page in browser
-            overlay: true ,
-            //contentBase ,
-          },
     }
+])
+
+const productionConfig = merge([]);
+
+const developmentConfig = merge([require("./webpack.parts/webpack.devServer")()]);
+
+module.exports = ({mode}) => {
+    console.log(mode);
+    console.dir(commonConfig);
+    console.dir(developmentConfig);
+    if (mode === "production") {
+        return merge([commonConfig, productionConfig, { mode } ]);
+    }
+        return merge([commonConfig, developmentConfig, { mode }]);
 }
---
+++ webpack.parts/webpack.devServer.js
@@ -0,0 +1,10 @@
+module.exports = () => ({
+    devServer: {
+        stats: "errors-only", // Display only errors to reduce the amount of output.
+        host: process.env.HOST, // Defaults to `localhost`
+        port: process.env.PORT || 3000, // Defaults to 8080
+        open: true, // Open the page in browser
+        overlay: true ,
+        //contentBase ,
+    },
+})
