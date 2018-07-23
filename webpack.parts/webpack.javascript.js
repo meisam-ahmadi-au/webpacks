d0c7715ecb02a0bcbbb650261a0e15505cae9c41
DESKTOP-PULU5LM\Meisam <meisam.ahmadi.1358@gmail.com>
2018-07-23 19:10:46 +0430

    add babel

---
+++ webpack.parts/webpack.javascript.js
@@ -0,0 +1,12 @@
+module.exports = ({ include, exclude } = {}) => ({
+    module: {
+        rules: [
+            {
+            test: /\.js$/,
+            include,
+            exclude: /node_modules/,
+            use: "babel-loader",
+            },
+        ],
+        },
+    });
